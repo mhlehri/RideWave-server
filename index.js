1a00134aba161f0412f4c48d8cbedb673e2630b8
mhlehri <lehrimirza101@gmail.com>
2023-11-08 09:33:38 +0600

    my services update & delete functionality emplemented

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ const port = process.env.PORT || 5000;
 // middleware
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yynznjj.mongodb.net/?retryWrites=true&w=majority`;
@@ -27,6 +32,20 @@ const client = new MongoClient(uri, {
 
 const servicesCollection = client.db("ServicesDB").collection("services");
 const bookingsCollection = client.db("ServicesDB").collection("bookings");
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorize. access denied!" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 async function run() {
   try {
@@ -57,8 +76,9 @@ async function run() {
 
     // getting my services
     try {
-      app.get("/myServices/:email", async (req, res) => {
+      app.get("/myServices/:email", verifyToken, async (req, res) => {
         const email = req.params.email;
+        console.log("owner", req.user, email, req.cookies);
         const services = servicesCollection.find({ providerEmail: email });
         const results = await services.toArray();
         res.send(results);
@@ -108,6 +128,36 @@ async function run() {
       console.log(error.message);
     }
 
+    //jwt
+    try {
+      app.post("/jwt", async (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+        res
+          .cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+          })
+          .send({ success: true });
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+
+    //jwt token remove
+    try {
+      app.post("/logout", async (req, res) => {
+        const user = req.body;
+        console.log("logged out");
+        res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+
     // Add Services
     try {
       app.post("/addServices", async (req, res) => {
@@ -122,17 +172,40 @@ async function run() {
     try {
       app.post("/addBookings", async (req, res) => {
         const booking = req.body;
+
         const result = await bookingsCollection.insertOne(booking);
         res.send(result);
       });
     } catch (error) {
       console.log(error.message);
     }
+    // update Bookings
+    try {
+      app.patch("/updateServices", async (req, res) => {
+        const providerEmail = req.body.user;
+        const query = { providerEmail };
+        console.log(providerEmail);
+        const update = {
+          $set: {
+            serviceImage: req.body?.serviceImage,
+            serviceName: req.body?.serviceName,
+            servicePrice: req.body?.servicePrice,
+            serviceArea: req.body?.serviceArea,
+            serviceDescription: req.body?.serviceDescription,
+          },
+        };
+        const result = await servicesCollection.updateOne(query, update);
+        res.send(result);
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+
     // remove Services
     try {
       app.delete("/services/:id", async (req, res) => {
         const id = req.params.id;
-        const result = await bookingsCollection.deleteOne({
+        const result = await servicesCollection.deleteOne({
           _id: new ObjectId(id),
         });
         res.send(result);
